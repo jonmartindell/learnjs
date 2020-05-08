9469c242c8dcdda45d3df5d896f4082b3da36ec7
Jonathan Martindell <jmartindell@covermymeds.com>
2020-05-08 14:35:36 -0400

    end of chp 3

---
+++ public/tests/app_spec.js
@@ -0,0 +1,58 @@
+describe("LearnJS", function () {
+  it("can show a problem view", function () {
+    learnjs.showView("#problem-1");
+    expect($(".view-container .problem-view").length).toEqual(1);
+  });
+
+  it("shows the landing page view when there is no hash", function () {
+    learnjs.showView("");
+    expect($(".view-container .landing-view").length).toEqual(1);
+  });
+
+  it("passes the hash view parameter to the view function", function () {
+    spyOn(learnjs, "problemView");
+    learnjs.showView("#problem-42");
+    expect(learnjs.problemView).toHaveBeenCalledWith("42");
+  });
+
+  it("invokes the router when loaded", function () {
+    spyOn(learnjs, "showView");
+    learnjs.appOnReady();
+    expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+  });
+
+  it("subscribes to the hash change event", function () {
+    learnjs.appOnReady();
+    spyOn(learnjs, "showView");
+    $(window).trigger("hashchange");
+    expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+  });
+
+  describe("problem view", function () {
+    var view;
+
+    beforeEach(function () {
+      view = learnjs.problemView("1");
+    });
+
+    it("has a title that includes the problem number", function () {
+      expect(view.text()).toMatch(/Problem #1 Coming soon!/);
+    });
+
+    describe("answer section", function () {
+      it("can check a correct answer by hitting a button", function () {
+        view.find(".answer").val("true");
+        view.find(".check-btn").click();
+        expect(view.find(".result").text().trim()).toEqual(
+          "Correct!Next Problem"
+        );
+      });
+
+      it("rejects an incorrect answer", function () {
+        view.find(".answer").val("false");
+        view.find(".check-btn").click();
+        expect(view.find(".result").text()).toEqual("Incorrect!");
+      });
+    });
+  });
+});
